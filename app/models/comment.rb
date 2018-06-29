3881d6ebf48d7259355984b4e3104eb70c4c439a
logictkt <logictkt@gmail.com>
2018-06-29 18:43:25 +0900

    ベース部分

---
+++ app/models/comment.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_comments_on_room_id  (room_id)
+#  index_comments_on_user_id  (user_id)
+#
+
+class Comment < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+end
---
+++ test/models/comment_test.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  room_id    :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_comments_on_room_id  (room_id)
+#  index_comments_on_user_id  (user_id)
+#
+
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
